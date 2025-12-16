84635deae2e93240c7d729a11609287b44e73996
PudimDeOvo <183882067+PudimDeOvo@users.noreply.github.com>
2025-12-16 18:55:34 -0300

    fix: small bug inside BubbleSort

--- trabalho4/BubbleSort.py
+++ trabalho4/BubbleSort.py
@@ -4,8 +4,8 @@ class BubbleSort:
         swaps = 0
         n = len(arr)
         for i in range(n):
-            comps += 1
             for j in range(0, n-i-1):
+                comps += 1
                 if arr[j] > arr[j+1]:
                     arr[j], arr[j+1] = arr[j+1], arr[j]
                     swaps += 1
